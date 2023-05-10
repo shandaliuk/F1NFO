434382d1a7cbaaf7fc24a05619109bdcef214af3
Kyrylo <shandaliuk3019@gmail.com>
2023-05-10 21:34:13 +0300

    Started to make current standings

--- src/components/CurrentStandingsSection/CurrentStandingsSection.jsx
+++ src/components/CurrentStandingsSection/CurrentStandingsSection.jsx
@@ -1,3 +1,43 @@
+import { useState } from 'react';
+import { useGetDriverStandingsQuery } from 'services/formulaOneApi/formulaOneApi';
+import { useGetConstructorStandingsQuery } from 'services/formulaOneApi/formulaOneApi';
+
 export const CurrentStandingsSection = () => {
-  return <section></section>;
+  const [currentStandings, setCurrentStandings] = useState(null);
+
+  const {
+    data: driversData,
+    isDriverDataLoading,
+    isDriverDataError,
+  } = useGetDriverStandingsQuery();
+
+  const {
+    data: constructorData,
+    isConstructorDataLoading,
+    isConstructorDataError,
+  } = useGetConstructorStandingsQuery();
+
+  return (
+    <section>
+      <h2>Current Standings</h2>
+      <ul>
+        <li>
+          <button type="button">Drivers</button>
+        </li>
+        <li>
+          <button type="button">Teams</button>
+        </li>
+      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>Pos</th>
+            <th>Name</th>
+            <th>Pts</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </section>
+  );
 };
